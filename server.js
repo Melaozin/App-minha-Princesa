a572cdc16d72f6c72aee1f9eee05d495e9fce1ee
Melaozin <132454734+Melaozin@users.noreply.github.com>
2024-11-02 00:10:41 +0000

    #

--- server.js
+++ server.js
@@ -7,37 +7,21 @@ const axios = require("axios");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 // Middleware para servir arquivos estáticos
 app.use(express.static("public"));
 
-// Rota para baixar o PDF
-app.get("/download", async (req, res) => {
-  const { downloadUrl, fileName } = req.query;
-
-  try {
-    const response = await axios.get(downloadUrl, { responseType: "stream" });
-    const filePath = path.join(__dirname, "public", "saved-books", fileName);
-    const writer = fs.createWriteStream(filePath);
-
-    response.data.pipe(writer);
-    writer.on("finish", () => res.sendStatus(200));
-    writer.on("error", () => res.sendStatus(500));
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+// Renderizar os arquivos
+// Página inicial
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-// Rota para listar os livros salvos
-app.get("/saved-books", (req, res) => {
-  const savedBooksDir = path.join(__dirname, "public", "downloads");
-
-  fs.readdir(savedBooksDir, (err, files) => {
-    if (err) {
-      return res.status(500).json({ error: "Erro ao listar livros salvos" });
-    }
-    res.json(files);
-  });
+// Meus livros
+app.get("/meus-livros", (req, res) => {
+  res.render("main");
 });
 
 // Iniciar o servidor
